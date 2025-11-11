dd998e7eb366b73d8cd1129750401300d25c8890
KonDax <lomasik553@gmail.com>
2025-11-11 17:36:20 +0300

    2.0.0

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import argparse
+import urllib.request
+import zipfile
+from zipfile import ZipFile
+
 
 class CLI:
     def __init__(self):
-        self.params = self.cmd_line()
-        self.print_args()
+        # self.params = self.cmd_line()
+        # self.print_args()
+        package_path = self.load_package_url("Microsoft.Extensions.DependencyInjection", "10.0.0")
+        self.load_packages(package_path)
 
 
     def cmd_line(self):
@@ -57,5 +63,26 @@ class CLI:
         print(f"package_version:\t\t{self.params['package_version']}")
         print(f"max_depth:\t{self.params['max_depth']}")
 
+    def load_package_url(self, package_name: str, version_name: str) -> str:
+        packages_url = f"https://www.nuget.org/api/v2/package/{package_name}/{version_name}"
+        print(f'load from {packages_url}')
+        with urllib.request.urlopen(packages_url) as response:
+            compressed_data = response.read()
+        package_path = f'{package_name}.{version_name}.nupkg'
+        with open(package_path, 'wb') as f:
+            f.write(compressed_data)
+        print(f"Сохранено в: {package_path}")
+        return package_path
+
+    def load_packages(self, package_path: str = ''):
+        file = ZipFile(package_path)
+        print(*file.namelist(), sep="\n")
+        print()
+        with file.open("Microsoft.Extensions.DependencyInjection.nuspec") as f:
+            print(*f.readlines(), sep="\n")
+
 
 cli = CLI()
+url1 = "https://www.nuget.org/api/v2/package/Newtonsoft.Json/13.0.4"
+url2 = "https://www.nuget.org/api/v2/package/System.Security.Cryptography.Pkcs/10.0.0"
+url3 = "https://www.nuget.org/api/v2/package/Microsoft.EntityFrameworkCore/10.0.0"
