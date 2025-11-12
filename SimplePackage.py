b1cde4423bf5506fcba7d204c8cbd704e15150c8
KonDax <lomasik553@gmail.com>
2025-11-12 11:04:15 +0300

    added dfs, test-mode

--- SimplePackage.py
+++ SimplePackage.py
@@ -5,17 +5,17 @@ import os
 
 class Package:
     def __init__(self, name: str, version: str , path: str = ""):
-        self.name = name
-        self.version = version
-        self.path = path
-        self.dependencies = []
+        self.name: str = name
+        self.version: str = version
+        self.path: str = path
+        self.dependencies: list[Package] = []
     def load_package_url(self, url: str) -> str | None:
         package_url = url + "/" + self.name + "/" + self.version
         print(f'load from {package_url}')
         try:
             with urllib.request.urlopen(package_url) as response:
                 compressed_data = response.read()
-            path = f'packages/{self.name}.{self.version}.nupkg'
+            path = f'{self.name}.{self.version}.nupkg'
             with open(path, 'wb') as f:
                 f.write(compressed_data)
             print(f"Сохранено в: {path}")
@@ -23,7 +23,8 @@ class Package:
             return path
         except Exception as e:
             print(f"Error: {e}")
-    def search_dependencies_by_path(self) -> list:
+
+    def get_dependencies_by_path(self) -> list:
         file_zip = ZipFile(self.path)
         # print(*file_zip.namelist(), sep="\n")
         # print()
@@ -37,13 +38,42 @@ class Package:
             # print(file_str)
         dependencies_raw = re.findall(r"<dependency .*>", file_str)
         # print(*dependencies_raw, sep="\n")
-        dependencies = []
         for dependence in dependencies_raw:
-            dependencies.append(re.findall(r'id=".*?"', dependence) + re.findall(r'version=".*?"', dependence))
-        return dependencies
+            package1 = Package(re.search(r'id=".*?"', dependence).group()[4:-1],
+                                 re.search(r'version=".*?"', dependence).group()[9:-1])
+            for package2 in self.dependencies:
+                comp = Package.compare(package2, package1)
+                if comp == "<" or comp == "==":
+                    self.dependencies.remove(package2)
+            self.dependencies.append(package1)
+        return self.dependencies
+
+    def get_dependencies_from_file(self, file_name) -> list | None:
+        with open(file_name, "r", encoding='utf-8') as f:
+            lines = f.read().strip().split('\n')
+        for line in lines:
+            if ':' in line:
+                package, deps = line.split(':', 1)
+                if package.strip() == self.name:
+                    self.dependencies = [Package(dep.strip(), "1.0.0") for dep in deps.strip().split(',') if dep.strip()]
+                    return self.dependencies
+        return None
+
+    @staticmethod
+    def compare(self, other):
+        if self.name != other.name:
+            return "!="
+        for digit1, digit2 in zip(self.version, other.version):
+            if "." not in [digit1, digit2] and int(digit1) < int(digit2):
+                return "<"
+            if "." not in [digit1, digit2] and int(digit1) > int(digit2):
+                return ">"
+        return "=="
+
     def clear_memory(self) -> None:
         try:
             if self.path != "":
                 os.remove(self.path)
+                path = ""
         except Exception as e:
             print(f"Error: {e}")
--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class CLI:
     def __init__(self):
         self.params = self.cmd_line()
         # self.print_args()
-        self.packages = []
+        self.graph_dependencies: dict[Package, list[Package]] = {}
+        self.visited: set[Package] = set()
 
     def cmd_line(self):
         params = {}
@@ -37,7 +38,7 @@ class CLI:
 
         parser.add_argument('--package-version', '-pv',
                             type=str,
-                            default="latest",
+                            required=True,
                             help="Package version"
                             )
 
@@ -64,19 +65,52 @@ class CLI:
         print(f"package_version:\t\t{self.params['package_version']}")
         print(f"max_depth:\t{self.params['max_depth']}")
 
-    def search_dependencies(self):
-        self.packages.append(Package(self.params["package_name"], self.params['package_version']))
-        for package in self.packages:
+    def get_dependencies(self, package: Package) -> list[Package]:
+        if self.params['work_mode']:
+            return package.get_dependencies_from_file(self.params["url"])
+        else:
             if package.load_package_url(self.params["url"]) is not None:
-                print(package.search_dependencies_by_path())
-            package.clear_memory()
+                deps = package.get_dependencies_by_path()
+                package.clear_memory()
+                return deps
+            return []
+    def recursive_dfs(self, start_package: Package, max_depth_recursive: int = 5) -> list[Package] | None:
+        for package in self.visited:
+            comp = Package.compare(start_package, package)
+            if comp == "==" or comp == "<":
+                return None
+            if comp == ">":
+                self.visited.remove(package)
+                del self.graph_dependencies[package]
+        # if start_package in self.visited:
+        #     return None
+        if max_depth_recursive <= 0:
+            return []
+        dependencies = self.get_dependencies(start_package)
+        # print(start_package.name, dependencies, self.visited, self.stack)
+        if not dependencies:
+            return []
+        self.visited.add(start_package)
+        for obj in dependencies:
+            deps = self.recursive_dfs(obj, max_depth_recursive-1)
+            if deps is not None:
+                self.graph_dependencies[start_package] = deps
+        self.graph_dependencies[start_package] = dependencies
+        return None
 
+    def get_graph_dependencies(self):
+        package = Package(self.params["package_name"], self.params["package_version"])
+        self.recursive_dfs(package, self.params["max_depth"])
+        for [key, value] in self.graph_dependencies.items():
+            print(f"{key.name}: {[i.name for i in value]}")
 
-# print(re.search(r'.*?.nuspec', "wethweth\nereryjeryj\nwetrhwerthwreth.nuspec"))
 url1 = "https://www.nuget.org/api/v2/package/Newtonsoft.Json/13.0.4"
 url2 = "https://www.nuget.org/api/v2/package/Microsoft.Extensions.DependencyInjection/10.0.0"
 url3 = "https://www.nuget.org/api/v2/package/Microsoft.EntityFrameworkCore/10.0.0"
+test1 = "python .\main.py -p A -u Test.txt -wd True -pv 1.0.0"
+test2 = "python .\main.py -p Microsoft.Extensions.DependencyInjection -u https://www.nuget.org/api/v2/package -pv 10.0.0 -d 3"
 if __name__ == "__main__":
     cli = CLI()
-    cli.search_dependencies()
+    cli.get_graph_dependencies()
+
 
